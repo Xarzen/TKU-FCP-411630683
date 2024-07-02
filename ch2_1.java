2b0285fe6421510ade0a475680da5e5a6aeef5ed
Xarzen <56926668+Xarzen@users.noreply.github.com>
2024-07-02 12:16:56 +0800

    Add files via upload

---
+++ ch2_1.java
@@ -0,0 +1,12 @@
+public class ch2_1{
+    public static void main (String[] args){
+        int num1=2;
+        int num2=3;
+        float num3=4;
+        String num4="三";
+        System.out.println(num2+" cats are running");
+        System.out.println(num1+" dogs are running");
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"ducks are running");
+    }
+}
