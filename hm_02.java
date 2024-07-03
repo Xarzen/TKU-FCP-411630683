440b22928aee732812a0b35053f5cc781e3bb881
Xarzen <raymondlei.twn@gmail.com>
2024-07-03 13:47:40 +0800

    0703練習題

---
+++ hm_02.java
@@ -0,0 +1,7 @@
+public class hm_02 {
+    public static void main(String[] args) {
+        System.out.println("名字: 雷天佑");
+        System.out.println("年齡: 21歲");
+        System.out.println("興趣愛好: 玩電腦、跳舞");
+    }
+}
