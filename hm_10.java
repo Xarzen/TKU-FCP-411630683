18aecc436034c3480678475bbf310afca72a5a33
Xarzen <56926668+Xarzen@users.noreply.github.com>
2024-07-17 07:29:08 +0800

    Update hm_10.java

--- hm_10.java
+++ hm_10.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class hm_10 {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -11,19 +10,4 @@ public class hm_10 {
         System.out.println("陣列元素的總和是: " + sum);
     }
 }
-=======
-import java.util.Scanner;
 
-public class hm_10 {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-
-        System.out.print("請輸入華氏溫度: ");
-        double fahrenheit = scn.nextDouble();
-
-        double celsius = (fahrenheit - 32) * 5 / 9;
-
-        System.out.println("攝氏溫度為: " + celsius);
-    }
-}
->>>>>>> 761827834366eab0f908b375c2b01400d1f3ab37
